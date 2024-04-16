0acc11a698a8f8832b23223f174be6887bf83d6c
Avishek <avishek.kdh@gmail.com>
2024-04-16 15:13:58 +0530

    Add user and related CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   root "pages#home"
   get '/about', to:'pages#about'
   resources :articles
+  get 'signup', to: "users#new"
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # Defines the root path route ("/")
   # root "posts#index"
 end
