42dad65e1a33f1c777aa2a9ca6e0e648fc8b8ed4
Avishek <avishek.kdh@gmail.com>
2024-04-26 12:48:54 +0530

    Add login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
 	def hello
 		render html: "hello world"
 	end 
+	
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
   end
   def create
     @article=Article.new(params.require(:article).permit(:title,:description))
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
